c25fd2d2d7c0a6d83afd161521a1e80ba26f81a0
JoshYacktman <joshyacktman@gmail.com>
2025-01-16 13:35:27 -0700

    Nail out the routing

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,53 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './../../public/css/main.css';
-import './../../public/css/utils.css';
-import './../../public/css/page_fills.css';
-
-
-function Header() {
-    function JoinButtonClicked () {
-        window.location.href='./../src/chat/';
-    }
-
-    function SignUpButtonClicked () {
-        window.location.href='./../chat/';
-    }
-
-    return (
-    <header className="metallic shadow_down">
-        <div className="splitter">
-            <div className="left push_left" style={{ paddingLeft: "0.5em"}}>
-                <h1 className="main_text medium">Provo Techspert</h1>
-            </div>
-            
-            <div className="right push_right">
-                <div className="dropdown">
-                    <button className="main_text normal shadow_down">Join/Sign In</button>
-                    <div className="hidden_div">
-                        <div className="dropdown_content">
-                            <form>
-                                <div className="form_format">
-                                    <label className="message_font normal">Username</label>
-                                    <input className="formal_font normal"/>
-                                    <label className="message_font normal">Password</label>
-                                    <input className="formal_font normal"/>
-                                    <label className="message_font normal">Email (Join Only)</label>
-                                    <input className="formal_font normal"/>
-                                </div>
-                                <div>
-                                    <button type="button" onClick={JoinButtonClicked}>Join</button>
-                                    <button type="button" onClick={SignUpButtonClicked}>Sign Up</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-  );
-}
+import bannerImg from "/images/banner.jpg";
+import promo1Img from "/images/promo_1.jpg";
+import promo2Img from "/images/promo_2.jpg";
+import profileImg from "/images/profile.jpg";
 
 
 function About() {
@@ -55,7 +11,7 @@ function About() {
         <div>
             {/* Banner */}
             <div className="banner">
-                <img src="public/images/banner.jpg" />
+                <img src={bannerImg} />
                 <h1 className="text_overlay complementary_font large">Provo Techspert</h1>
             </div>
 
@@ -63,12 +19,12 @@ function About() {
             {/* Why Me? */}
             <div className="splitter picture_left" style={{backgroundColor: "var(--contrast_color_1)"}}>
                 <div className="left center">
-                    <img src="public/images/promo_1.jpg" className="promo_photo rounded shadow_down" />
+                    <img src={promo1Img} className="promo_photo rounded shadow_down" />
                 </div>
                 <div className="right index_text center">
                     <h1 className="complementary_font large">Why Me?</h1>
                     <div className="paragraph_list">
-                        <p className="formal_font normal" style={{paddingTop: 1.5 + "em"}}>1. I don't keep your device while replacement parts are being delivered</p>
+                        <p className="formal_font normal" style={{paddingTop: 1.5 + "em"}}>1. I don"t keep your device while replacement parts are being delivered</p>
                         <p className="formal_font normal">2. You choose my commision based on my performance and only have to pay for replacement parts</p>
                         <p className="formal_font normal">3. You can choose to cancel the repair process at any point</p>
                         <p className="formal_font normal">4. You can watch and be a part of every step</p>
@@ -78,7 +34,7 @@ function About() {
                     <div className="paragraph_list">
                         <p className="formal_font normal">1. You have to sign a release of liability waiver before repairs can begin</p>
                         <p className="formal_font normal">2. If you choose to purchase the parts and not go through with the repair there are no refunds</p>
-                        <p className="formal_font normal">3. Use your real email, the chat service sends email notifications after messages and doesn't sell any data</p>
+                        <p className="formal_font normal">3. Use your real email, the chat service sends email notifications after messages and doesn"t sell any data</p>
                     </div>
                 </div>
             </div>
@@ -96,7 +52,7 @@ function About() {
                     </div>
                 </div>
                 <div className="right center">
-                    <img src="public/images/promo_2.jpg" className="promo_photo rounded shadow_down" />
+                    <img src={promo2Img} className="promo_photo rounded shadow_down" />
                     {/* TODO: Make this a picture of the chat screen with a user account */}
                 </div>
             </div>
@@ -105,12 +61,12 @@ function About() {
             {/* About Me */}
             <div className="splitter picture_left" style={{backgroundColor: "var(--contrast_color_3)"}}>
                 <div className="left center">
-                    <img src="public/images/profile.jpg" className="promo_photo rounded shadow_down" />
+                    <img src={profileImg} className="promo_photo rounded shadow_down" />
                 </div>
                 <div className="right index_text center">
                     <h1 className="complementary_font large">Meet the Technician</h1>
                     <p className="formal_font medium" style={{paddingTop: 0.5 + "em"}}>Joshua Yacktman</p>
-                    <p className="formal_font normal" style={{paddingTop: 2 + "em"}}>I am a Freshman at BYU studying Computer Science with a passion for both hardware and software alike. Starting at a young age I found great enjoyment in solving puzzles and taking things apart. This has led to passions such as competetive Rubik's Cube solving, programming, and (as you're likely here for) tech repair!</p>
+                    <p className="formal_font normal" style={{paddingTop: 2 + "em"}}>I am a Freshman at BYU studying Computer Science with a passion for both hardware and software alike. Starting at a young age I found great enjoyment in solving puzzles and taking things apart. This has led to passions such as competetive Rubik"s Cube solving, programming, and (as you"re likely here for) tech repair!</p>
                 </div>
             </div>
         </div>
---
+++ src/components.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+function JoinButtonClicked () {
+    window.location.href = "/chat/";
+}
+
+function SignInButtonClicked () {
+    window.location.href = "/chat/";
+}
+
+function SignOutButtonClicked () {
+    window.location.href = "/";
+}
+
+function ChatButtonClicked () {
+    window.location.href = "/chat/";
+}
+
+function SettingsButtonClicked () {
+    window.location.href = "/settings/";
+}
+
+
+function AboutDropdownOptions () {
+    return (
+        <form>
+            <div className="form_format">
+                <label className="message_font normal">Username</label>
+                <input className="formal_font normal"/>
+                <label className="message_font normal">Password</label>
+                <input className="formal_font normal"/>
+                <label className="message_font normal">Email (Join Only)</label>
+                <input className="formal_font normal"/>
+            </div>
+            <div style={{flexDirection: "row"}}>
+                <button type="button" onClick={JoinButtonClicked}>Join</button>
+                <button type="button" onClick={SignInButtonClicked}>Sign In</button>
+            </div>
+        </form>
+    );
+}
+
+function ChatDropdownOptions () {
+    return (
+        <div>
+            <button onClick={SettingsButtonClicked}>Settings</button>
+            <button onClick={SignOutButtonClicked}>Sign Out</button>
+        </div>
+    );
+}
+
+function SettingsDropdownOptions () {
+    return (
+        <div>
+            <button onClick={ChatButtonClicked}>Chat</button>
+            <button onClick={SignOutButtonClicked}>Sign Out</button>
+        </div>
+    );
+}
+
+function Dropdown () {
+    var location = useLocation().pathname;
+    return (
+        <div className="dropdown">
+            <button className="main_text normal shadow_down">Join/Sign In</button>
+            <div className="hidden_div">
+                <div className="dropdown_content">
+                    { location === "/" ? <AboutDropdownOptions /> : location === "/chat/" ? <ChatDropdownOptions /> : <SettingsDropdownOptions />}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function Header () {
+    return (
+        <header className="metallic shadow_down">
+            <div className="splitter">
+                <div className="left push_left" style={{ paddingLeft: "0.5em"}}>
+                    <h1 className="main_text medium">Provo Techspert</h1>
+                </div>
+                <div className="right push_right">
+                    <Dropdown />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+
+export function Footer() {
+    function GitHubButtonClicked () {
+        window.open("https://github.com/JoshYacktman/Provo-Techspert");
+    }
+
+    return (
+    <footer className="metallic shadow_up">
+        <div className="push_left">
+            <button className="complementary_font normal shadow_up" onClick={GitHubButtonClicked}>GitHub</button>
+        </div>
+    </footer>
+  );
+}
+
+export function NotFound() {
+    return <main className="center" style={{padding: 2+"em"}}>404: Address unknown. Try going to simply https://ProvoTechspert.click</main>;
+}

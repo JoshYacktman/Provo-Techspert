ed3f72796e8b41379856babeb4f088c397d812f4
JoshYacktman <joshyacktman@gmail.com>
2025-01-16 13:00:11 -0700

    React routing

---
+++ src/chat/chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Chat () {
+    return (
+        <div className="work_area">
+            <h1>Chat</h1>
+            <p>app components</p>
+            <p>websocket chat component</p>
+            <p style={{paddingBottom: .5 + "em"}}>Database inference</p>
+        </div>
+    );
+}
+export default Chat;

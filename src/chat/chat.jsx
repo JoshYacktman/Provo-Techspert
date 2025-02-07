c0356f36ab9f1636e6de085368824707e2bda866
JoshYacktman <joshyacktman@gmail.com>
2025-02-07 10:59:41 -0700

    Continue work on chat

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { Dropup } from "../components";
 import "./chat.css";
 
@@ -19,22 +19,37 @@ function ChatDropdownOptions() {
 }
 
 function Chat() {
-    const openSidebar = (event) => {
-        event.stopPropagation();
-        document.getElementById("sidebar").style.width = "auto";
-    }
-    const closeSidebar = () => {
-        document.getElementById("sidebar").style.width = "0";
-    }
+    const [openStatus, setOpenStatus] = useState(false);
 
-    const messagesEndRef = useRef(null);
+    const toggleStatus = () => {
+        setOpenStatus(!openStatus);
+    };
+
+    const hoverEnterStatus = () => {
+        setOpenStatus(true);
+    };
+
+    const hoverExitStatus = (event) => {
+        const relatedTarget = event.relatedTarget;
+        if (
+            !relatedTarget ||
+            !(relatedTarget instanceof Node) ||
+            (!event.currentTarget.contains(relatedTarget) &&
+                relatedTarget.closest(".dropup") === null)
+        ) {
+            setOpenStatus(false);
+        }
+    };
+
 
     // Function to scroll to the bottom
+    const messagesEndRef = useRef(null);
+
     const scrollToBottomInstant = () => {
         messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
     };
     const scrollToBottomSmooth = () => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "instant" });
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
 
     useEffect(() => {
@@ -43,10 +58,10 @@ function Chat() {
 
     return (
         <div className="work_area">
-            <div className="sidebar shadow_down" id="sidebar" style={{ height: "100%", display: "flex", flexDirection: "column" }}>
+            <div className="sidebar shadow_down" id="sidebar" style={{ height: "100%", display: "flex", flexDirection: "column", width: (openStatus ? "auto" : "0")}} onMouseLeave={hoverExitStatus}>
                 <div style={{width: "100%", display: "flex", flexDirection: "row", zIndex: "1"}} className="shadow_down">
                     <button style={{ width: "100%"}} className="normal bordered">Create Chat</button>
-                    <button className="normal bordered" onClick={closeSidebar}>&larr;</button>
+                    <button className="normal bordered" onClick={toggleStatus}>&larr;</button>
                 </div>
                 <div style={{ height: "100%", overflowY: "auto"}}>
                     <div style={{width: "100%", display: "flex", flexDirection: "row"}}>
@@ -98,37 +113,110 @@ function Chat() {
             </div>
             <div style={{ height: "100%", display: "flex", flexDirection: "column" }}>
                 <div className="shadow_down" style={{ display: "flex", flexDirection: "row", alignItems: "center", zIndex: "1"}}>
-                    <button className="small shadow_down" onClick={(event) => openSidebar(event)}>&equiv;</button>
+                    <button className="small shadow_down" onMouseEnter={hoverEnterStatus}>&equiv;</button>
                     <div style={{ flex: 1, display: "flex", justifyContent: "center" }}>
                         <h1 className="meduim main_text">Chat One</h1>
                     </div>
                 </div>
                 <div style={{ height: "100%", overflowX: "hidden", overflowY: "auto", backgroundColor: "yellowgreen"}}>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message one</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message two</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message three</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message four</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message five</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message six</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message seven</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message eight</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message nine</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message ten</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message eleven</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message twelve</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message thirteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message fourteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message fifteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message sixteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message seventeen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message eighteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", color: "purple"}}>Provo Techspert: Chat Message nineteen</p>
-                    <p className="normal message_font" style={{paddingLeft: "0.5em", paddingRight: "0.5em", paddingBottom: "0.3em", paddingTop: "0.3em", textAlign: "right", color: "darkblue"}}>Customer: Chat Message twenty</p>
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message one</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message two</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message three</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message four</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message five</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message six</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message seven</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message eight</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message nine</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message ten</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message eleven</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message twelve</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message thirteen</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message fourteen</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message fifteen</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message sixteen</p>
+                    </div>
+
+                    <div style={{ padding: "0.5em", color: "purple" }}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Provo Techspert</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message seventeen</p>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message eighteen</p>
+                    </div>
+
+
+                    <div style={{ padding: "0.5em", textAlign: "right", color: "darkblue"}}>
+                        <h5 className="normal message_font" style={{filter: "brightness(85%)"}}>Username</h5>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message nineteen</p>
+                        <p className="small message_font" style={{ paddingTop: "0.1em" }}>Chat Message twenty</p>
+                    </div>
+
                     <div ref={messagesEndRef}></div>
                 </div>
                 <div  className="shadow_up" style={{width: "100%", display: "flex", flexDirection: "row", zIndex: "1"}}>
                     <input className="small message_font" style={{ width: "100%" }}></input>
-                    <button>&rarr;</button>
+                    <button onClick={scrollToBottomSmooth}>&rarr;</button>
                 </div>
             </div>
         </div>

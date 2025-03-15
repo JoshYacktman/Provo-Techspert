b082b2c1f89280e8cf1cb7b973cf8e56c1eeb6b8
JoshYacktman <joshyacktman@gmail.com>
2025-03-15 00:48:34 -0600

    Further work on service

--- service/index.js
+++ service/index.js
@@ -1,15 +1,20 @@
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 // const bcrypt = require("bcrypt");
 const express = require("express");
 // const uuid = require("uuid");
 const app = express();
 
-// const authCookieName = "token";
+const authCookieName = "token";
 
-// let authTokens = {};
+let authTokens = {};
 // key is token, value is username
+let tokenDates = {};
+// Key is date and value is token. If a token is a week old we delete it
+// using a cron job that runs once a day (node-cron)
 
-// let users = {};
+let users = {};
+// By searching for users here instead of in a list we get O(1) lookup
+// and it serves dual purposes
 // Example Users:
 // {
 //   "Provo Techspert": {
@@ -20,7 +25,7 @@ const app = express();
 //   }
 // }
 
-// let chats = {};
+let chats = {};
 // Example chats:
 // {
 //   "UserOne": {
@@ -63,23 +68,65 @@ const app = express();
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const build_loc = process.argv.length > 2 ? "public" : "../dist";
 
 app.use(express.json());
-// app.use(cookieParser());
-app.use(express.static("../dist"));
+app.use(cookieParser());
+app.use(express.static(build_loc));
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use("/api", apiRouter);
 
-app.get("/api/test", (req, res) => {
-  console.log(req.originalUrl);
+apiRouter.get("/test", (req, res) => {
   res.send("Hello, Test!");
 });
 
-// Catch-all route for serving index.html
+function createUser(username, password) {
+  users[username] = true;
+  // TODO: Unfinished!
+}
+
+apiRouter.get("/auth/create", async (req, res) => {
+  res.send("Hello, Create!");
+  if (req.body.username in users) {
+    res.status(409).send({ msg: "Existing user" });
+    return;
+  }
+  const user = await createUser(req.body.username, req.body.password);
+  setAuthCookie(res, user);
+  res.send("Authorized");
+});
+
+apiRouter.get("/auth/login", async (req, res) => {
+  res.send("Hello, Login!");
+  const user = await findUser(req.body.username);
+  const authorization = await bcrypt.compare(
+    req.body.password,
+    user.password_hash,
+  );
+  if (!user || !authorization) {
+    res.status(409).send({ msg: "Not authorized" });
+    return;
+  }
+  setAuthCookie(res, user);
+  res.send("Authorized");
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, user) {
+  user.token = uuid.v4();
+
+  res.cookie(authCookieName, user.token, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+}
+
+// Catch-all route for serving index.html (main page)
 app.use((req, res) => {
-  res.sendFile("index.html", { root: "../dist" });
+  res.sendFile("index.html", { root: build_loc });
 });
 
 app.listen(port, () => {
